e5cb9be03e78485755cdc2224d66f89e41f2e945
nihad1213 <nihad.nemetli@gmail.com>
2024-12-22 19:48:15 +0400

    Task class created

--- task.rb
+++ task.rb
@@ -1,1 +1,13 @@
-# Task Class
+class Task
+  attr_accessor :title, :due_date, :is_complete
+
+  def initialize(title, due_date)
+    @title = title
+    @due_date = due_date
+    @is_complete = false
+  end
+  
+  def complete
+    @is_complete = !@is_complete
+  end
+end
