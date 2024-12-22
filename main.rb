2cc0d2419f3f06b5e0c944b6c8c3278d7ce4873d
nihad1213 <nihad.nemetli@gmail.com>
2024-12-22 20:03:51 +0400

    Task is finished

--- main.rb
+++ main.rb
@@ -1,30 +1,59 @@
-require "rainbow/refinement"
+require 'rainbow/refinement'
+require_relative 'to_do_list'
 using Rainbow
+
+# Initialize the ToDoList
+to_do_list = ToDoList.new
 
 loop do
   puts "Choose an option: "
-  puts "1 =>Add Task"
-  puts "2 => View Task"
+  puts "1 => Add Task"
+  puts "2 => View Tasks"
   puts "3 => Delete Task"
   puts "4 => Mark Task as Complete/Incomplete"
   puts "5 => Exit"
   print "Enter your choice: "
   choice = gets.to_i
 
-  if choice == 1
-    puts "1".green
-  elsif choice == 2
-    puts "2".green
-  elsif choice == 3
-    puts "3".green
-  elsif choice == 4
-    puts "4".green
-  elsif choice == 5
+  case choice
+  when 1
+    # Add Task
+    print "Enter task title: "
+    title = gets.chomp
+    print "Enter due date (YYYY-MM-DD): "
+    due_date = gets.chomp
+    to_do_list.add_task(title, due_date)
+    puts "Task added!".green
+  when 2
+    # View Tasks
+    puts "Tasks:"
+    to_do_list.list_tasks
+  when 3
+    # Delete Task
+    print "Enter task number to delete: "
+    task_number = gets.to_i
+    if task_number.between?(1, to_do_list.instance_variable_get(:@tasks).size)
+      to_do_list.delete_task(task_number - 1)
+      puts "Task deleted!".green
+    else
+      puts "Invalid task number.".red
+    end
+  when 4
+    # Mark Task as Complete/Incomplete
+    print "Enter task number to mark as complete/incomplete: "
+    task_number = gets.to_i
+    if task_number.between?(1, to_do_list.instance_variable_get(:@tasks).size)
+      task = to_do_list.instance_variable_get(:@tasks)[task_number - 1]
+      task.is_complete = !task.is_complete2
+      puts "Task marked as #{task.is_complete ? 'Complete' : 'Incomplete'}!".green
+      to_do_list.save_tasks
+    else
+      puts "Invalid task number.".red
+    end
+  when 5
     puts "Closing Program.".green
     break
   else
     puts "Wrong Operation!".red
   end
 end
-
-
--- to_do_list.rb
+++ to_do_list.rb
@@ -1,1 +1,50 @@
-# To Do List class
+require 'json'
+# Load task.rb file
+require_relative 'task'
+
+class ToDoList
+  def initialize(file = 'tasks.json')
+    # Empty array that holds all tasks
+    @tasks = []
+    # Default filename is tasks.json
+    @file = file
+    load_tasks
+  end
+
+  def add_task(title, due_date)
+    @tasks << Task.new(title, due_date)
+    save_tasks
+  end
+
+  def delete_task(index)
+    @tasks.delete_at(index)
+    save_tasks
+  end
+
+  def list_tasks
+    # Simply print out the list of tasks
+    @tasks.each_with_index do |task, index|
+      status = task.is_complete ? '[✔]' : '[ ]'
+      puts "#{index + 1}. #{status} #{task.title} (Due: #{task.due_date})"
+    end
+  end
+
+  def save_tasks
+    # Convert tasks to an array of hashes and save to a file
+    tasks_data = @tasks.map do |task|
+      { title: task.title, due_date: task.due_date, is_complete: task.is_complete }
+    end
+    File.write(@file, JSON.pretty_generate(tasks_data))
+  end
+
+  def load_tasks
+    return unless File.exist?(@file)
+
+    tasks_data = JSON.parse(File.read(@file))
+    @tasks = tasks_data.map do |data|
+      task = Task.new(data['title'], data['due_date'])
+      task.is_complete = data['is_complete']
+      task
+    end
+  end
+end
